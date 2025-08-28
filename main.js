a4c65580733d606d5f9ff46ad5119ca7e19d6693
FSHimel <shahriarfahim067@gmail.com>
2025-08-28 23:58:44 +0600

    call btn, call history input and heart count done

---
+++ main.js
@@ -0,0 +1,353 @@
+const callHistoryData = [];
+const callHistoryContainer = document.getElementById("call-history-container");
+// call button functionalities
+document.getElementById("call-1").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-1").innerText;
+    const number =document.getElementById("number-1").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+document.getElementById("call-2").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-2").innerText;
+    const number = document.getElementById("number-2").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+document.getElementById("call-3").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-3").innerText;
+    const number = document.getElementById("number-3").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+document.getElementById("call-4").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-4").innerText;
+    const number = document.getElementById("number-4").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+document.getElementById("call-5").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-5").innerText;
+    const number =document.getElementById("number-5").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+document.getElementById("call-6").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-6").innerText;
+    const number = document.getElementById("number-6").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+document.getElementById("call-7").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-7").innerText;
+    const number =document.getElementById("number-7").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+document.getElementById("call-8").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-8").innerText;
+    const number = document.getElementById("number-8").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+document.getElementById("call-9").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const name = document.getElementById("name-9").innerText;
+    const number =document.getElementById("number-9").innerText
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if(coins < 20){
+        alert("You don't have enough coins to make this call")
+    }
+    else{
+        const totalAvailableCoins = coins - 20;
+        document.getElementById("coins").
+        innerText = totalAvailableCoins;
+        alert("📞 Calling "+name+ "  " +":" +"  " + number+"...");
+
+        const data = {
+            Name: name,
+            Number: number,
+            Date:new Date().toLocaleTimeString()
+        }
+        callHistoryData.push(data);
+
+        const div = document.createElement("div")
+        div.innerHTML=`
+            <div class="bg-[#fafafa] rounded-[8px] flex justify-between items-center mt-[8px] p-[16px] md:max-w-[352px]">
+                <div>
+                    <h2 class="text-[18px] font-semibold hind-madurai">${data.Name}</h2>
+                    <p class="text-[18px] font-normal hind-madurai text-[#5c5c5c]">${data.Number}</p>
+                </div>
+                <p class="text-[18px] font-normal">${data.Date}</p>
+            </div>
+    `
+    callHistoryContainer.appendChild(div);
+    }
+    
+    
+})
+
+
+// heart section 
+
+const hearts = document.getElementsByClassName("heart")
+console.log(hearts)
+for(let i=0; i<hearts.length; i++){
+        hearts[i].addEventListener("click", function(){
+        const heartCount = parseInt(document.getElementById("heart-count").innerText);
+        const finalHeartCount = heartCount + 1;
+        document.getElementById("heart-count").
+        innerText = finalHeartCount;
+    })
+}
+
+
+
